9bcec9a96e326acea388fefbd1802aabef1bef70
phfmatias <pedrohfmatias@gmail.com>
2024-06-13 15:56:08 -0300

    Refactor plot_species method in LAMMPS_Analyser.py for improved readability and efficiency

--- renderVMD.py
+++ renderVMD.py
@@ -12,6 +12,7 @@
 ##   Usage:        ['python3 renderVMD.py wait=False <input_file>']
 
 from sys import argv
+from pyperclip import copy
 try:
     from MoleKing import PeriodicTable, G16LOGfile, Molecule
 except ImportError:
@@ -20,8 +21,14 @@ except ImportError:
 
 import os
 
-if len(argv) != 3:
+if len(argv) != 3 and argv[2].split('.')[1] != 'cub':
     print("Usage: python3 renderVMD.py wait=False <input_file>")
+    print("wait=False: Do not wait for the VMD window to close")
+    print("wait=True: Wait for the VMD window to close, so you can rotate the molecule")
+    exit()
+
+if argv[2].split('.')[-1] == 'cub' and len(argv) != 4:
+    print("Usage: python3 renderVMD.py wait=False <cube_file> <isovalue>")
     print("wait=False: Do not wait for the VMD window to close")
     print("wait=True: Wait for the VMD window to close, so you can rotate the molecule")
     exit()
@@ -31,7 +38,7 @@ class Render():
         self.input_file = input_file
         self.elements = {}
         self.toRemove = ''
-        self. colorsVMD = [x for x in range(32)]
+        self.colorsVMD = [x for x in range(32)]
 
         self.name = self.input_file.split('.')[0]
             
@@ -62,19 +69,28 @@ class Render():
             exit()
 
     def check_file(self):
-        if self.input_file.split('.')[-1] not in ['gjf', 'xyz', 'log']:
+        if self.input_file.split('.')[-1] not in ['gjf', 'xyz', 'log', 'cub']:
+            print(self.input_file)
             print("Invalid file format. Please provide a valid file format: gjf, xyz or log")
             exit()
 
-        elif self.input_file.split('.')[-1] == 'xyz':
+        if self.input_file.split('.')[-1] == 'xyz':
             self.doXYZ()
+            self.ipt = 'xyz'
 
         if self.input_file.split('.')[-1] == 'gjf':
             self.doGJF()
+            self.ipt = 'gjf'
 
-        elif self.input_file.split('.')[-1] == 'log':
+        if self.input_file.split('.')[-1] == 'log':
             self.doLOG()
+            self.ipt = 'log'
 
+        if self.input_file.split('.')[-1] == 'cub':
+            self.doCUB()
+            self.ipt = 'cub'
+
+        
     def doGJF(self):
         mol = Molecule()
         arq = open(self.input_file, 'r').readlines()
@@ -98,6 +114,20 @@ class Render():
             self.input_file = 'temp.xyz'
             self.doXYZ()
 
+    
+    def doCUB(self):
+        self.AtomsCub = []
+        arq = open(self.input_file, 'r').readlines()
+        for i in range(len(arq)):
+            if len(arq[i].split()) == 5:
+                self.AtomsCub.append(arq[i].split()[0])
+
+        self.AtomsCub = list(set(self.AtomsCub))
+
+        for i in self.AtomsCub:
+            atomCub = PeriodicTable().getSymbol(int(i))
+            self.elements[atomCub] = self.HEX2RGB(PeriodicTable().getColor(atomCub))
+
     def doLOG(self):
         x = G16LOGfile(self.input_file).getMolecule().toXYZ(fileName='temp.xyz')
         self.input_file = 'temp.xyz'
@@ -120,54 +150,172 @@ class Render():
                 print(i, PeriodicTable().getColor(i), self.HEX2RGB(PeriodicTable().getColor(i)))
             self.elements[i] = self.HEX2RGB(PeriodicTable().getColor(i))
 
-    def doTCL(self):
-        arq = open('render.tcl', 'w')
-        arq.write('mol representation {CPK 1.0 0.3 50.0 30.0}\n')
-        arq.write('mol addrep top\n')
-        arq.write('mol modcolor 1 0 Element\n')
-        c = 0
-        for key, value in self.elements.items():
-            arq.write('color Element {} {}\n'.format(key, self.colorsVMD[c]))
-            arq.write('color change rgb {} {:.6f} {:.6f} {:.6f}\n'.format(c, value[0], value[1], value[2]))
-            c+=1
-        arq.write('material change mirror Opaque 0.15\n')
-        arq.write('material change outline Opaque 4.000000\n')
-        arq.write('material change outlinewidth Opaque 0.5\n')
-        arq.write('material change ambient Glossy 0.1\n')
-        arq.write('material change diffuse Glossy 0.600000\n')
-        arq.write('material change opacity Glossy 0.75\n')
-        arq.write('material change ambient Opaque 0.08\n')
-        arq.write('material change mirror Opaque 0.0\n')
-        arq.write('material change shininess Glossy 1.0\n')
-        arq.write('display distance -7.0\n')
-        arq.write('display height 10\n')
-        arq.write('light 3 on\n')
-        arq.write('display depthcue off\n')
-        arq.write('color Display Background white\n')
-        arq.write('axes location off\n')
-        arq.write('display update\n')
-        if not self.wait:
-            arq.write('render Tachyon vmdscene.dat\n')
-            arq.write('exit')
+    def doTCL(self, ipt):
+
+        if ipt in ['gjf', 'log', 'xyz']:
+            arq = open('render.tcl', 'w')
+            arq.write('mol representation {CPK 1.0 0.3 50.0 30.0}\n')
+            arq.write('mol addrep top\n')
+            arq.write('mol modcolor 1 0 Element\n')
+            c = 0
+            for key, value in self.elements.items():
+                arq.write('color Element {} {}\n'.format(key, self.colorsVMD[c]))
+                arq.write('color change rgb {} {:.6f} {:.6f} {:.6f}\n'.format(c, value[0], value[1], value[2]))
+                c+=1
+            arq.write('material change mirror Opaque 0.15\n')
+            arq.write('material change outline Opaque 4.000000\n')
+            arq.write('material change outlinewidth Opaque 0.5\n')
+            arq.write('material change ambient Glossy 0.1\n')
+            arq.write('material change diffuse Glossy 0.600000\n')
+            arq.write('material change opacity Glossy 0.75\n')
+            arq.write('material change ambient Opaque 0.08\n')
+            arq.write('material change mirror Opaque 0.0\n')
+            arq.write('material change shininess Glossy 1.0\n')
+            arq.write('display distance -7.0\n')
+            arq.write('display height 10\n')
+            arq.write('light 3 on\n')
+            arq.write('display depthcue off\n')
+            arq.write('color Display Background white\n')
+            arq.write('axes location off\n')
+            arq.write('display update\n')
+            if not self.wait:
+                arq.write('render Tachyon vmdscene.dat\n')
+                arq.write('exit')
+
+        if ipt == 'cub':
+            arq = open('showcub.vmd','w')
+            arq.write('proc cub {filename {isoval 0.05}} {\n')
+            arq.write('set mater Glossy\n')
+            arq.write('color Display Background white\n')
+            arq.write('display depthcue off\n')
+            arq.write('display rendermode GLSL\n')
+            arq.write('axes location Off\n')
+            c = 0
+            for key, value in self.elements.items():
+                print(key, value, self.colorsVMD[c])
+                arq.write('color Name {} {}\n'.format(key, self.colorsVMD[c]))
+                arq.write('color change rgb {} {:.6f} {:.6f} {:.6f}\n'.format(c, value[0], value[1], value[2]))
+                c+=1
+            arq.write('material change mirror Opaque 0.15\n')
+            arq.write('material change outline Opaque 4.000000\n')
+            arq.write('material change outlinewidth Opaque 0.5\n')
+            arq.write('material change ambient Glossy 0.1\n')
+            arq.write('material change diffuse Glossy 0.600000\n')
+            arq.write('material change opacity Glossy 0.75\n')
+            arq.write('material change shininess Glossy 1.0\n')
+            arq.write('light 3 on\n')
+            arq.write('foreach i [molinfo list] {\n')
+            arq.write('mol delete $i\n')
+            arq.write('}\n')
+            arq.write('mol new $filename.cub\n')
+            arq.write('mol modstyle 0 top CPK 0.800000 0.300000 22.000000 22.000000\n') #LIGAÇÃO
+            arq.write('mol addrep top\n')
+            arq.write('mol modstyle 1 top Isosurface $isoval 0 0 0 1 1\n')
+            arq.write('color change rgb {} 0.000000 0.000000 1.00000 \n'.format(len(self.colorsVMD)))
+            arq.write('mol modcolor 1 top ColorID {}\n'.format(len(self.colorsVMD)))          #AZUL POSITIVO
+            arq.write('mol modmaterial 1 top $mater\n')
+            arq.write('mol addrep top\n')
+            arq.write('mol modstyle 2 top Isosurface -$isoval 0 0 0 1 1\n')
+            arq.write('color change rgb {} 1.000000 1.000000 0.000000 \n'.format(len(self.colorsVMD) -1))
+            arq.write('mol modcolor 2 top ColorID {}\n'.format(len(self.colorsVMD) -1))    #AMARELO NEGATIVO
+            arq.write('mol modmaterial 2 top $mater\n')
+            arq.write('display distance -8.0\n')
+            arq.write('display height 10\n')
+            arq.write('}\n')
+            arq.write('proc cubiso {isoval} {\n')
+            arq.write('mol modstyle 1 top Isosurface $isoval 0 0 0 1 1\n')
+            arq.write('mol modstyle 2 top Isosurface -$isoval 0 0 0 1 1\n')
+            arq.write('}\n')
+            arq.write('proc cub2 {filename1 filename2 {isoval 0.05}} {\n')
+            arq.write('set mater Glossy\n')
+            arq.write('color Display Background white\n')
+            arq.write('display depthcue off\n')
+            arq.write('display rendermode GLSL\n')
+            arq.write('axes location Off\n')
+            c = 0
+            for key, value in self.elements.items():
+                print(key, value, self.colorsVMD[c])
+                arq.write('color Name {} {}\n'.format(key, self.colorsVMD[c]))
+                arq.write('color change rgb {} {:.6f} {:.6f} {:.6f}\n'.format(c, value[0], value[1], value[2]))
+                c+=1
+            arq.write('material change mirror Opaque 0.15\n')
+            arq.write('material change outline Opaque 4.000000\n')
+            arq.write('material change outlinewidth Opaque 0.5\n')
+            arq.write('material change ambient Glossy 0.1\n')
+            arq.write('material change diffuse Glossy 0.600000\n')
+            arq.write('material change opacity Glossy 0.75\n')
+            arq.write('material change shininess Glossy 1.0\n')
+            arq.write('light 3 on\n')
+            arq.write('foreach i [molinfo list] {\n')
+            arq.write('mol delete $i\n')
+            arq.write('}\n')
+            arq.write('mol new $filename1.cub\n')
+            arq.write('mol modstyle 0 top CPK 0.800000 0.300000 22.000000 22.000000\n')
+            arq.write('mol addrep top\n')
+            arq.write('mol modstyle 1 top Isosurface $isoval 0 0 0 1 1\n')
+            arq.write('mol modcolor 1 top ColorID 12\n')
+            arq.write('mol modmaterial 1 top $mater\n')
+            arq.write('mol new $filename2.cub\n')
+            arq.write('mol modstyle 0 top CPK 0.800000 0.300000 22.000000 22.000000\n')
+            arq.write('mol addrep top\n')
+            arq.write('mol modstyle 1 top Isosurface $isoval 0 0 0 1 1\n')
+            arq.write('mol modcolor 1 top ColorID 22\n')
+            arq.write('mol modmaterial 1 top $mater\n')
+            arq.write('display distance -8.0\n')
+            arq.write('display height 10\n')
+            arq.write('}\n')
+            arq.write('proc cub2iso {isoval} {\n')
+            arq.write('foreach i [molinfo list] {\n')
+            arq.write('mol modstyle 1 $i Isosurface $isoval 0 0 0 1 1\n')
+            arq.write('}\n')
+            arq.write('}\n')
+            arq.write('cub {} {}\n'.format(self.input_file.split('.')[0], argv[3]))
+            if not self.wait:
+                arq.write('render Tachyon vmdscene.dat\n')
+                arq.write('exit')
     
     def doRender(self):
-        self.doTCL()
+        self.doTCL(self.ipt)
         
-        if self.wait:
-            print('Please, when you are done type this in the vmd terminal:')
-            print('render Tachyon vmdscene.dat')
-            print('exit')
-            input('Press Enter to continue to open VMD')
+        if self.ipt in ['gjf', 'log', 'xyz']:
+            if self.wait:
+                print('Please, when you are done, just paste in the VMD terminal. The code was already copied to the clipboard.')
+                print('exit')
+                input('Press Enter to continue to open VMD')
+                x = copy('render Tachyon vmdscene.dat')
+                os.system('vmd {} -e render.tcl'.format(self.input_file))
+                os.system('tachyon vmdscene.dat -format PNG -o {}.png -res 2000 1500 -aasamples 24'.format(self.name))
+                os.system('rm render.tcl')
 
-        os.system('vmd {} -e render.tcl'.format(self.input_file))
-        os.system('tachyon vmdscene.dat -format PNG -o {}.png -res 2000 1500 -aasamples 24'.format(self.name))
+            else:
+                print('Rendering...')
+                os.system('vmd {} -e render.tcl'.format(self.input_file))
+                os.system('tachyon vmdscene.dat -format PNG -o {}.png -res 2000 1500 -aasamples 24'.format(self.name))
+                self.toRemove = 'render.tcl vmdscene.dat'
+                os.system('rm {}'.format(self.toRemove))
+        
+            if 'temp.xyz' in os.listdir():
+                os.system('rm temp.xyz')
 
-        #self.toRemove = 'render.tcl vmdscene.dat'
-        #os.system('rm {}'.format(self.toRemove))
-    
-        if 'temp.xyz' in os.listdir():
-            os.system('rm temp.xyz')
+        else:
+            if self.wait:
+                print('Please, when you are done, just paste in the VMD terminal. The code was already copied to the clipboard.')
+                print('exit')
+                input('Press Enter to continue to open VMD')
+                x = copy('render Tachyon vmdscene.dat')
+                os.system('vmd {} -e showcub.vmd'.format(self.input_file))
+                os.system('tachyon vmdscene.dat -format PNG -o {}.png -res 2000 1500 -aasamples 24'.format(self.name))
+                os.system('rm render.tcl')
+            else:
+                print('Rendering...')
+                os.system('vmd {} -e showcub.vmd'.format(self.input_file.split('.')[0]))
+                os.system('tachyon vmdscene.dat -format PNG -o {}.png -res 2000 1500 -aasamples 24'.format(self.name))
+                self.toRemove = 'showcub.vmd vmdscene.dat'
+                os.system('rm {}'.format(self.toRemove))
+            
+            if 'temp.xyz' in os.listdir():
+                os.system('rm temp.xyz')
 
 
 if __name__ == '__main__':
-    Render(argv[-1])
+    Render(argv[2])
